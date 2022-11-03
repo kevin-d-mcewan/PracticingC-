be65b5f9adcdff4568618db73dc547b2758ea97c
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2022-11-03 13:17:01 -0700

    adding drawing a circle in the window and moving it with the keys

--- vscode-template-main/axe_game.cpp
+++ vscode-template-main/axe_game.cpp
@@ -2,15 +2,47 @@
 
 int main(int argc, char const *argv[])
 {
-  /* code */
-  int width = 1000;
+  // Colors
+  Color windowBG_Color = RED;
+  Color gameBG_Color = GREEN;
+  Color primary_Color = DARKGRAY;
+  Color secondary_Color = VIOLET;
 
-  InitWindow(width, 750, "Kevin's Window");
+  /* Window Dimensions */
+  int width = 1000;
+  int height{750};
+
+  // Circle Coordinates/Dimensions
+  int circle_x{width/2};
+  int circle_y{height/2};
+  int circle_radius{width / 3};
+
+  InitWindow(width, height, "Kevin's Window");
+  SetTargetFPS(60);
   while (WindowShouldClose() == false)
   {
     BeginDrawing();
-    ClearBackground(RED);
-    DrawCircle(350, 350, 100, BLUE);
+    ClearBackground(windowBG_Color);
+
+    // Game Logic Begins
+    DrawCircle(circle_x, circle_y, circle_radius, secondary_Color);
+
+    if (IsKeyDown(KEY_D))
+    {
+      /* Moving the Circle*/
+      circle_x = circle_x + 10;
+    }
+
+    if (IsKeyDown(KEY_A))
+    {
+      /* Moving Circle Left*/
+      circle_x = circle_x - 10;
+    }
+    
+    
+
+
+    // Game Logic Ends
     EndDrawing();
   }
 }
